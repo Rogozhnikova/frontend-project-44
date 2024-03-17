572922c65ca1d9d28447bca23809e78ddcb33c24
Alina Rogozhnikova <linarogozhnikova30@gmail.com>
2024-03-17 22:43:52 +0500

    added the "Parity Check" game

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import userName from '../src/cli.js';
+import { welcomeUser } from '../src/cli.js';
 
-userName;
+welcomeUser();
--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,8 @@
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!');
-const userName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${userName}!`);
-
-export default { userName };
+export function welcomeUser() {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  return userName;
+}
