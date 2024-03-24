891a44f40147ad00baab8f085d1d48843aed6880
Alina Rogozhnikova <linarogozhnikova30@gmail.com>
2024-03-25 03:02:43 +0500

    corrections

--- src/games/logEven.js
+++ src/games/logEven.js
@@ -1,5 +1,4 @@
 import startGame from '../index.js';
-import randomNumber from '../randomNumber.js';
 
 function isEven(number) {
   return number % 2 === 0;
@@ -8,6 +7,7 @@ function isEven(number) {
 const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 function getQuestionAndAnswer() {
+  const randomNumber = Math.floor(Math.random() * 100);
   const question = `${randomNumber}`;
   const answer = isEven(question) ? 'yes' : 'no';
   return [question, answer];
